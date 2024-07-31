e2b3f52b298fa589d8bfd29998cd390fc99a5e11
JDW <xs@jdw.sx>
2024-07-30 21:45:55 -0400

    Add union type to const enum comment test case.

--- tests/php81_files/src/026_enum_class_constant.php
+++ tests/php81_files/src/026_enum_class_constant.php
@@ -7,6 +7,10 @@ enum A26 {
 class B26 {
 	/** @var A26 */
 	public const Valid = A26::Single;
+
+    /** @var array<int|string, array<int, string|A26>> */
+    public const AlsoValid = [ [ A26::Single ] ];
 }
 
 echo B26::Valid->name, "\n";
+echo B26::AlsoValid[0][0]->name, "\n";
